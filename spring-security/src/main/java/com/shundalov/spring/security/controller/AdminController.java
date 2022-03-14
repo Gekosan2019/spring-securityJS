a04c2dc9c7c532cab48cf3be9f7408cb7e22ca27
Shamil <super.shomka@mail.ru>
2022-03-15 01:40:13 +0300

    newUpdate

---
+++ spring-security/src/main/java/com/shundalov/spring/security/controller/AdminController.java
@@ -0,0 +1,38 @@
+package com.shundalov.spring.security.controller;
+
+import com.shundalov.spring.security.model.Role;
+import com.shundalov.spring.security.model.User;
+import com.shundalov.spring.security.service.RoleService;
+import com.shundalov.spring.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Controller
+public class AdminController {
+
+    private UserService userService;
+    private RoleService roleService;
+
+    @Autowired
+    private void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    private void setRoleService(RoleService roleService){
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/admin")
+    public String allUsers()  {
+        return "admin.html";
+    }
+
+}
